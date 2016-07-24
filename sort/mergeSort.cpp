c465ee85f2474a9ed45d44c7f969885d19b99db3
liliangmin <1508490934@qq.com>
2016-07-24 23:45:14 +0800

    natural merge sort

--- sort/mergeSort.cpp
+++ sort/mergeSort.cpp
@@ -23,9 +23,34 @@ void mergeSortRecursive(int a[], int reg[], int start, int end){
     a[k] = reg[k];
 }
 
+void naturalMergeSortRecursive(int a[], int reg[], int start, int end){
+  if(start >= end)
+    return;
+  int mid = start;
+  while(mid < end && a[mid] <= a[mid+1])
+    mid ++;
+  if(mid == end)
+    return;
+  int start1 = start;
+  int end1 = mid;
+  int start2 = mid + 1;
+  int end2 = end;
+  naturalMergeSortRecursive(a, reg, start1, end1);
+  naturalMergeSortRecursive(a, reg, start2, end2);
+  int k = start;
+  while(start1 <= end1 && start2 <= end2)
+    reg[k++] = a[start1] <= a[start2] ? a[start1++] : a[start2++];
+  while(start1 <= end1)
+    reg[k++] = a[start1++];
+  while(start2 <= end2)
+    reg[k++] = a[start2++];
+  for(k = start; k <= end; k ++)
+    a[k] = reg[k];
+}
+
 void mergeSort(int a[], int n){
   int reg[n];
-  mergeSortRecursive(a, reg, 0, n - 1);
+  naturalMergeSortRecursive(a, reg, 0, n - 1);
 }
 
 int main(){
